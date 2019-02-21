34b438eac104bde277f4a6b941e6e348e422a6e9
Jason <jason.jin1223@gmail.com>
2019-02-20 23:37:59 -0800

    fixed bug with nonstandard messages, and allowed registering commands for dynamically adding/removing commands

--- BaseBot.py
+++ BaseBot.py
@@ -14,10 +14,11 @@ PING = "PING :tmi.twitch.tv\r\n"
 class BaseBot:
     def __init__(self):
         self.conn = None
+        self.command_dict = {}
         self.connect_to_channel()
         self.run_main()
 
-    """Method uses the configurable global variables to connect to the Twitch IRC Server
+    """ Method uses the configurable global variables to connect to the Twitch IRC Server
     """
     def connect_to_channel(self):
         self.conn = socket.socket()
@@ -26,36 +27,42 @@ class BaseBot:
         self.conn.send("NICK {}\r\n".format(NICK).encode("utf-8"))
         self.conn.send("JOIN #{}\r\n".format(CHAN).encode("utf-8"))
 
+    """ Main Loop
+    """
     def run_main(self):
         response = ""
         while True:
             response = response + self.conn.recv(1024).decode("utf-8")
-            print("response: ")
-            print(response)
-            print("\n")
             all_data = response.split("\r\n")
             response = all_data.pop()
             status, user, message = self.parse_message(all_data[0])
             if status:
-                return
+                continue
             status = self.handle_message(user, message)
 
+    """ Allows for registering commands
+        Arguments:
+            - cmd: type: str The string to look for ex "!test"
+            - cmd_handler: type: method The method to run when this command is entered
+    """
+    def register_command(self, cmd, cmd_handler):
+        if cmd not in self.command_dict.keys():
+            self.command_dict[cmd] = cmd_handler
 
-    def handle_message(self, user, message):
-        if message[0] == "!":
-            # need to check the message against the commands
-            data = message.split(" ")
-            if data[0] in self.registered_commands:
-                # run command
-                self.run_command[data[0]]
-            else:
-                pass
-        else:
-            pass
+    """ De-registers a command so that it is no longer usable.
+    """
+    def deregister_command(self, cmd):
+        if cmd in self.command_dict.keys():
+            self.command_dict.pop(cmd)
 
+    """ Runs a command from the registered command_dict
+    """
     def run_command(self, cmd):
-        pass
+        self.command_dict[cmd]()
 
+    """ Sends a message to the chat group.
+        Defaults to the configured channel, but can also send messages to other channels.
+    """
     def send_message(self, msg, user=CHAN):
         message = "PRIVMSG #%s :%s\r\n" % (user, msg)
         self.conn.send(message.encode("utf-8"))
@@ -88,7 +95,25 @@ class BaseBot:
             message = data[-1]
             print(message)
             return 0, username, message
+        return 1, -1,-1
 
+    """ Function to read the message and determine whether it is a command.
+        Non command messages are ignored.
+    """
+    def handle_message(self, user, message):
+        if message[0] == "!":
+            # need to check the message against the commands
+            data = message.split(" ")
+            if data[0] in self.command_dict.keys():
+                # run command
+                return self.run_command(data[0])
+            else:
+                return 1
+        else:
+            return 1
+
+    """ The following methods are standard twitch chat commands for mods.
+    """
     def ban(self, user):
         self.conn.send(".ban %s" % user)
 
