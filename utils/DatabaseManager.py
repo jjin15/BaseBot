ae4c2af35b1985edd531870510eb0ac7232eabe3
Jason <jason.jin1223@gmail.com>
2019-03-28 19:15:12 -0700

    restructured files

---
+++ utils/DatabaseManager.py
@@ -0,0 +1,88 @@
+import sqlite3
+
+DB_ERROR = 1
+
+
+class DatabaseManager:
+    def __init__(self, fn):
+        self.conn = None
+        self.cur = None
+        try:
+            self.connect(fn)
+        except:
+            pass
+
+    def connect(self, fn):
+        self.conn = sqlite3.connect(fn)
+        self.cur = self.conn.cursor()
+
+    """ Function will check whether or not a username exists in the database
+        Returns DB_ERROR if there was an issue executing the query
+        Returns False if user does not exist
+        Returns name of user if user exists
+    """
+    def check_user_exists(self, uname):
+        try:
+            self.cur.execute("SELECT name FROM Users WHERE name=?", (str(uname),))
+        except Exception as e:
+            print("ERROR" + str(e))
+            return DB_ERROR
+        res = self.cur.fetchone()
+        # user does not exist
+        if res is None:
+            return False
+        return res[0]
+
+    """ Adds a user to the database, with default 0 points
+        Returns False if the user does not exist
+        Returns True if operation successful
+    """
+    def add_user(self, uname, points=0):
+        if self.check_user_exists(uname):
+            return False
+        try:
+            data = (uname, points)
+            self.cur.execute("INSERT into Users (name, points) VALUES (?,?)", data)
+        except Exception as e:
+            print ('exception in add_user: ' + str(e))
+            return False
+        self.conn.commit()
+        return True
+
+    """ Helper function that retrieves the number of points a certain user has
+        returns False if the user does not exist
+        returns the number of points (int) if user exists
+    """
+    def get_user_points(self, uname):
+        if not self.check_user_exists(uname):
+            return False
+        try:
+            data = (uname,)
+            self.cur.execute("SELECT points FROM Users WHERE name=?", data)
+            res = int(self.cur.fetchone()[0])
+        except Exception as e:
+            print("exception in get_user_points: " + str(e))
+            return False
+        return res
+
+    """ Function to modify the number of points a user has
+        Returns False if user does not exist
+        returns True if operation successful
+    """
+    def change_user_points(self, uname, points):
+        if not self.check_user_exists(uname):
+            return False
+        # need to pull current points
+        res = self.get_user_points(uname)
+        if not res:
+            return False
+        # update the new number of points
+        new_res = res + points
+        try:
+            data = (new_res, uname)
+            self.cur.execute("UPDATE Users SET points=? WHERE name=?", data)
+        except Exception as e:
+            print ('exception in change_user_points_update: ' + str(e))
+            return False
+        self.conn.commit()
+        return True
---
+++ utils/MusicPlayer.py
@@ -0,0 +1,86 @@
+import vlc
+import pafy
+from collections import deque
+
+INVALID_URL_ERR = 'Invalid URL Entered'
+EMPTY_QUEUE_ERR = 'No Songs in Queue'
+
+
+class Song:
+    def __init__(self, title, length, url):
+        self.title = title
+        self.length = length
+        self.url = url
+
+    def get_title(self):
+        return self.title
+
+    def get_length(self):
+        return self.length
+
+    def get_url(self):
+        return self.url
+
+
+class MusicPlayer:
+    def __init__(self):
+        self.queue = deque()
+        self.current_play_thread = None
+        self.playing = False
+        self.instance = vlc.Instance()
+        self.player = self.instance.media_player_new()
+        self.media = None
+        self.current_song = None
+
+    def add_to_queue(self, url):
+        try:
+            url = pafy.new(url)
+        except (ValueError, OSError):
+            return INVALID_URL_ERR
+        best = url.getbestaudio()
+        best_url = best.url
+        title = url.title
+        length = url.duration
+        s = Song(title, length, best_url)
+        self.queue.append(s)
+        return 0
+
+    def play_track(self):
+        if self.playing:
+            self.play()
+            return 0
+        self.playing = True
+        if not len(self.queue) > 0:
+            return EMPTY_QUEUE_ERR
+        self.current_song = self.queue.popleft()
+        self.media = self.instance.media_new(self.current_song.get_url())
+        self.media.get_mrl()
+        self.player.set_media(self.media)
+        self.player.play()
+        return 0
+
+    def get_current_title(self):
+        if self.current_song is not None:
+            return self.current_song.get_title()
+        return False
+
+    def get_current_song_duration(self):
+        if self.current_song is not None:
+            return self.current_song.get_length()
+        return False
+
+    def play(self):
+        self.player.play()
+
+    def pause_track(self):
+        self.player.pause()
+
+    def stop_track(self):
+        self.player.stop()
+        self.current_song = None
+        self.playing = False
+        self.media = None
+
+    def skip_track(self):
+        self.stop_track()
+        return self.play_track()
